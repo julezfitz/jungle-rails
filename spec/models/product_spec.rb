89488387943a3ff68a80bf3f495dee9adacf7636
Julie Fitzpatrick <juliemfitzpatrick@gmail.com>
2022-01-19 16:34:58 -0330

    this commit finishes testing for products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
    
-    it "validates :name, presence: true" do
+    it "validates successful create" do
       cat1 = Category.find_or_create_by! name: 'Test'
       @product = cat1.products.create!({
         :name => 'Thingy', 
@@ -15,47 +15,51 @@ RSpec.describe Product, type: :model do
         :category_id => 1 
       })
 
-      name = @product.name ? true: false;
-      expect(name).to be true
+      expect(@product.errors.full_messages).to be_empty
     end
 
     it "validates :price, presence: true" do
       cat1 = Category.find_or_create_by! name: 'Test'
-      @product = cat1.products.create!({
+      @product = cat1.products.create({
         :name => 'Thingy', 
-        :price => 100, 
         :image => 'apparel6.jpg',
         :quantity => 100,
       })
 
-      price = @product.price ? true: false;
-      expect(price).to be true
+      expect(@product.errors.full_messages).to include ('Price can\'t be blank')
     end
 
     it "validates :quantity, presence: true" do
       cat1 = Category.find_or_create_by! name: 'Test'
-      @product = cat1.products.create!({
+      @product = cat1.products.create({
         :name => 'Thingy', 
         :price => 100, 
         :image => 'apparel6.jpg',
-        :quantity => 100,
       })
 
-      quantity = @product.quantity ? true: false;
-      expect(quantity).to be true
+      expect(@product.errors.full_messages).to include ('Quantity can\'t be blank')
     end
 
     it "validates :category, presence: true" do
-      cat1 = Category.find_or_create_by! name: 'Test'
-      @product = cat1.products.create!({
+      @product = Product.create({
         :name => 'Thingy', 
         :price => 100, 
         :image => 'apparel6.jpg',
         :quantity => 100,
       })
 
-      category = @product.category_id ? true: false;
-      expect(category).to be true
+      expect(@product.errors.full_messages).to include ('Category can\'t be blank')
+    end
+
+    it "validates :name, presence: true" do
+      cat1 = Category.find_or_create_by! name: 'Test'
+      @product = cat1.products.create({
+        :price => 1000, 
+        :image => 'apparel6.jpg',
+        :quantity => 5,
+      })
+      
+      expect(@product.errors.full_messages).to include ('Name can\'t be blank')
     end
   end
 end
